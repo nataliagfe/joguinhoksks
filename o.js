47371962a6ed35d3cdf6e3973cd124391ddd8251
nataliagfe <nataliaogfe@gmail.com>
2024-10-27 19:47:19 -0300

    Add files via upload

---
+++ o.js
@@ -0,0 +1,26 @@
+
+function showPopup(message) {
+    document.getElementById("popup-text").innerText = message;
+    document.getElementById("overlay").style.display = "flex";
+}
+
+function closePopup() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function funcaoDesenho() {
+    window.location.href = "desenho.html"; }
+
+function voltar() {
+    window.location.href = "index.html"; }
+
+function senha1() {
+    window.location.href = "desenho1.html"; }
+
+
+
+function funcaoCofre() {
+    window.location.href = "cofre.html"; }
+
+function funcaoCofre2() {
+    window.location.href = "cofre2.html"; }
